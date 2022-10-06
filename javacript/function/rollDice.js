a989728c424adde648e520a99635115b83d14bfd
Johan Testas <johan.ts.dev@gmail.com>
2022-10-06 11:17:58 +0200

    javascript done

---
+++ javacript/function/rollDice.js
@@ -0,0 +1,82 @@
+function rollDice() {
+  let diceNumber = Math.trunc(Math.random() * 6) + 1;
+  const diceDots = document.querySelectorAll(".dice div");
+
+  switch (diceNumber) {
+    case 1:
+      diceDots[0].style.backgroundColor = "#FFF";
+      diceDots[1].style.backgroundColor = "#FFF";
+      diceDots[2].style.backgroundColor = "#FFF";
+      diceDots[3].style.backgroundColor = "#FFF";
+      diceDots[4].style.backgroundColor = "#ea412b";
+      diceDots[5].style.backgroundColor = "#FFF";
+      diceDots[6].style.backgroundColor = "#FFF";
+      diceDots[7].style.backgroundColor = "#FFF";
+      diceDots[8].style.backgroundColor = "#FFF";
+      break;
+      
+    case 2:
+      diceDots[0].style.backgroundColor = "#FFF";
+      diceDots[1].style.backgroundColor = "#FFF";
+      diceDots[2].style.backgroundColor = "#ea412b";
+      diceDots[3].style.backgroundColor = "#FFF";
+      diceDots[4].style.backgroundColor = "#FFF";
+      diceDots[5].style.backgroundColor = "#FFF";
+      diceDots[6].style.backgroundColor = "#ea412b";
+      diceDots[7].style.backgroundColor = "#FFF";
+      diceDots[8].style.backgroundColor = "#FFF";
+      break;
+
+    case 3:
+      diceDots[0].style.backgroundColor = "#FFF";
+      diceDots[1].style.backgroundColor = "#FFF";
+      diceDots[2].style.backgroundColor = "#ea412b";
+      diceDots[3].style.backgroundColor = "#FFF";
+      diceDots[4].style.backgroundColor = "#ea412b";
+      diceDots[5].style.backgroundColor = "#FFF";
+      diceDots[6].style.backgroundColor = "#ea412b";
+      diceDots[7].style.backgroundColor = "#FFF";
+      diceDots[8].style.backgroundColor = "#FFF";
+      break;
+
+    case 4:
+      diceDots[0].style.backgroundColor = "#ea412b";
+      diceDots[1].style.backgroundColor = "#FFF";
+      diceDots[2].style.backgroundColor = "#ea412b";
+      diceDots[3].style.backgroundColor = "#FFF";
+      diceDots[4].style.backgroundColor = "#FFF";
+      diceDots[5].style.backgroundColor = "#FFF";
+      diceDots[6].style.backgroundColor = "#ea412b";
+      diceDots[7].style.backgroundColor = "#FFF";
+      diceDots[8].style.backgroundColor = "#ea412b";
+      break;
+
+    case 5:
+      diceDots[0].style.backgroundColor = "#ea412b";
+      diceDots[1].style.backgroundColor = "#FFF";
+      diceDots[2].style.backgroundColor = "#ea412b";
+      diceDots[3].style.backgroundColor = "#FFF";
+      diceDots[4].style.backgroundColor = "#ea412b";
+      diceDots[5].style.backgroundColor = "#FFF";
+      diceDots[6].style.backgroundColor = "#ea412b";
+      diceDots[7].style.backgroundColor = "#FFF";
+      diceDots[8].style.backgroundColor = "#ea412b";
+      break;
+
+    case 6:
+      diceDots[0].style.backgroundColor = "#ea412b";
+      diceDots[1].style.backgroundColor = "#FFF";
+      diceDots[2].style.backgroundColor = "#ea412b";
+      diceDots[3].style.backgroundColor = "#ea412b";
+      diceDots[4].style.backgroundColor = "#FFF";
+      diceDots[5].style.backgroundColor = "#ea412b";
+      diceDots[6].style.backgroundColor = "#ea412b";
+      diceDots[7].style.backgroundColor = "#FFF";
+      diceDots[8].style.backgroundColor = "#ea412b";
+      break;
+  }
+
+  return diceNumber;
+}
+
+export {rollDice};
---
+++ javacript/main.js
@@ -0,0 +1,103 @@
+import {rollDice} from "./function/rollDice.js";
+
+//DOM elements
+const global1 = document.getElementById("global--0");
+const global2 = document.getElementById("global--1");
+
+const current1 = document.getElementById("current--0");
+const current2 = document.getElementById("current--1");
+
+const dice = document.querySelector(".dice");
+const diceDots = document.querySelectorAll(".dice div");
+
+const btnNew = document.querySelector(".btn-new-game"); 
+const btnRoll = document.querySelector(".btn-roll");
+const btnHold = document.querySelector(".btn-hold"); 
+
+const player1Dot = document.querySelector(".player1 h2 span");
+const player2Dot = document.querySelector(".player2 h2 span");
+
+const player1Zone = document.querySelector(".player1");
+const player2Zone = document.querySelector(".player2");
+
+//Function
+
+function switchPlayer() {
+  current = 0;
+  document.getElementById(`current--${activePlayer}`).textContent  = current;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player1Zone.classList.toggle("player1-active")
+  player2Zone.classList.toggle("player2-active")
+}
+
+
+//Initialization
+let scores = [0, 0];
+let current = 0;
+let activePlayer = 0;
+
+// implement btnRoll functionality
+btnRoll.addEventListener("click", () => {
+
+  //1. generate a random number [1 to 6] and display the dice
+  let diceNumber = rollDice();
+  dice.style.display = "flex";
+
+  //2. add diceNumber to current if it is not equal 1
+  if (diceNumber !== 1) {
+    current += diceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent  = current;
+  }
+  //3. change active player and set current to 0 if diceNumber is equal 1
+  else {
+    switchPlayer();
+  }
+})
+
+// implement btnHold functionality
+btnHold.addEventListener("click", () => {
+  //1. add current to global score
+  scores[activePlayer] += current;
+  document.getElementById(`global--${activePlayer}`).textContent  = scores[activePlayer];
+
+  //2. check if the player has reached the maximum
+  if (scores[activePlayer] >= 10) {
+    activePlayer === 0 ? player1Zone.classList.add("winner-player") : player1Zone.classList.add("looser-player")
+    activePlayer === 1 ? player2Zone.classList.add("winner-player") : player2Zone.classList.add("looser-player")
+    
+    btnHold.style.display = "none";
+    btnRoll.style.display = "none";
+  } 
+  
+  else {
+    switchPlayer();
+    diceDots.forEach(dot => dot.style.backgroundColor = "#FFF");
+  }
+})
+
+
+// implement btnNew functionality
+
+btnNew.addEventListener("click", () => {
+
+  scores = [0, 0];
+  current = 0;
+  activePlayer = 0;
+
+  global1.textContent = 0;
+  global2.textContent = 0;
+  current1.textContent = 0;
+  current2.textContent = 0;
+
+  player1Zone.classList.add("player1-active");
+  player2Zone.classList.remove("player2-active");
+
+  player1Zone.classList.remove("winner-player");
+  player2Zone.classList.remove("winner-player");
+  player1Zone.classList.remove("looser-player");
+  player2Zone.classList.remove("looser-player");
+
+  btnHold.style.display = "flex";
+  btnRoll.style.display = "flex";
+})
+
