7102e626db48b4d8c7eb656a77fd27c431520eb5
Johan Testas <johan.ts.dev@gmail.com>
2022-10-09 16:10:44 +0200

    new css mobile first

--- javacript/main.js
+++ javacript/main.js
@@ -1,7 +1,6 @@
 //Import functions
 import {rollDice} from "./function/rollDice.js";
 
-
 //DOM elements
 const global1 = document.getElementById("global--0");
 const global2 = document.getElementById("global--1");
@@ -22,6 +21,8 @@ const player2Dot = document.querySelector(".player2 h2 span");
 const player1Zone = document.querySelector(".player1");
 const player2Zone = document.querySelector(".player2");
 
+const player1Name = document.querySelector(".player1 h2");
+const player2Name = document.querySelector(".player2 h2");
 
 //Initialization
 let scores;
@@ -31,18 +32,17 @@ initialize();
 
 //Implement btnRoll functionality
 btnRoll.addEventListener("click", () => {
-
+  
   //1. generate a number between [ 1 and 6 ] and display the dice
   let diceNumber = rollDice();
-  dice.style.display = "flex";
 
-  //2. add diceNumber to current if it is not equal 1
+  //3. add diceNumber to current if it is not equal 1
   if (diceNumber !== 1) {
     current += diceNumber;
     document.getElementById(`current--${activePlayer}`).textContent  = current;
   }
 
-  //3. change active player and set current to 0 if diceNumber is equal 1
+  //4. change active player and set current to 0 if diceNumber is equal 1
   else {
     switchPlayer();
   }
@@ -55,20 +55,23 @@ btnHold.addEventListener("click", () => {
   scores[activePlayer] += current;
   document.getElementById(`global--${activePlayer}`).textContent  = scores[activePlayer];
 
-  //2. check if the player HAS reached the maximum
-  if (scores[activePlayer] >= 100) {
-    activePlayer === 0 ? player1Zone.classList.add("winner-player") : player1Zone.classList.add("looser-player")
-    activePlayer === 1 ? player2Zone.classList.add("winner-player") : player2Zone.classList.add("looser-player")
-    
+  //2. if the player HAS reached the maximum
+  if (scores[activePlayer] >= 10) {
+    activePlayer === 0 ? player1Zone.classList.add("winner-player") : player1Zone.classList.add("looser-player");
+    activePlayer === 1 ? player2Zone.classList.add("winner-player") : player2Zone.classList.add("looser-player");
+
+    activePlayer === 0 ? player1Name.innerHTML = "WINNER !" : player2Name.innerHTML = "WINNER !";
+
     btnHold.style.display = "none";
     btnRoll.style.display = "none";
   } 
   
-  //3. check if the player HAS NOT reached the maximum
+  //3. if the player HAS NOT reached the maximum
   else {
     switchPlayer();
-    diceDots.forEach(dot => dot.style.backgroundColor = "#FFF");
+    clearDice();
   }
+
 })
 
 //Implement btnNew functionality
@@ -86,23 +89,30 @@ function initialize() {
   current1.textContent = 0;
   current2.textContent = 0;
 
-  player1Zone.classList.add("player1-active");
-  player2Zone.classList.remove("player2-active");
+  player1Zone.classList.add("active-player");
+  player2Zone.classList.remove("active-player");
 
   player1Zone.classList.remove("winner-player");
   player2Zone.classList.remove("winner-player");
   player1Zone.classList.remove("looser-player");
   player2Zone.classList.remove("looser-player");
 
-  btnHold.style.display = "flex";
-  btnRoll.style.display = "flex";
-  dice.style.display = "none";
+  clearDice();
+  btnHold.style.display = "block"
+  btnRoll.style.display = "block"
+
+  player1Name.innerHTML = "PLAYER 1"; 
+  player2Name.innerHTML = "PLAYER 2";
 }
 
 function switchPlayer() {
   current = 0;
   document.getElementById(`current--${activePlayer}`).textContent  = current;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player1Zone.classList.toggle("player1-active");
-  player2Zone.classList.toggle("player2-active");
+  player1Zone.classList.toggle("active-player");
+  player2Zone.classList.toggle("active-player");
+}
+
+function clearDice() {
+  diceDots.forEach(dot => dot.style.backgroundColor = "#FFF");
 }
