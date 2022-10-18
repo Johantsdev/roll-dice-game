4352ce59be870ae1d02a34b68d5ab814dc20dd31
Johan Testas <johan.ts.dev@gmail.com>
2022-10-18 09:33:48 +0200

    Arrange repeated code in rollDice function with a new function applying the colors of the dots

--- javacript/function/rollDice.js
+++ javacript/function/rollDice.js
@@ -8,79 +8,42 @@ function rollDice() {
   
   switch (diceNumber) {
     case 1:
-      diceDots[0].style.backgroundColor = "#FFF";
-      diceDots[1].style.backgroundColor = "#FFF";
-      diceDots[2].style.backgroundColor = "#FFF";
-      diceDots[3].style.backgroundColor = "#FFF";
-      diceDots[4].style.backgroundColor = "#ea412b";
-      diceDots[5].style.backgroundColor = "#FFF";
-      diceDots[6].style.backgroundColor = "#FFF";
-      diceDots[7].style.backgroundColor = "#FFF";
-      diceDots[8].style.backgroundColor = "#FFF";
+      dotsColor(diceDots, [4]);
       break;
       
     case 2:
-      diceDots[0].style.backgroundColor = "#FFF";
-      diceDots[1].style.backgroundColor = "#FFF";
-      diceDots[2].style.backgroundColor = "#ea412b";
-      diceDots[3].style.backgroundColor = "#FFF";
-      diceDots[4].style.backgroundColor = "#FFF";
-      diceDots[5].style.backgroundColor = "#FFF";
-      diceDots[6].style.backgroundColor = "#ea412b";
-      diceDots[7].style.backgroundColor = "#FFF";
-      diceDots[8].style.backgroundColor = "#FFF";
+      dotsColor(diceDots, [2,6]);
       break;
 
     case 3:
-      diceDots[0].style.backgroundColor = "#FFF";
-      diceDots[1].style.backgroundColor = "#FFF";
-      diceDots[2].style.backgroundColor = "#ea412b";
-      diceDots[3].style.backgroundColor = "#FFF";
-      diceDots[4].style.backgroundColor = "#ea412b";
-      diceDots[5].style.backgroundColor = "#FFF";
-      diceDots[6].style.backgroundColor = "#ea412b";
-      diceDots[7].style.backgroundColor = "#FFF";
-      diceDots[8].style.backgroundColor = "#FFF";
+      dotsColor(diceDots, [2,4,6]);
       break;
 
     case 4:
-      diceDots[0].style.backgroundColor = "#ea412b";
-      diceDots[1].style.backgroundColor = "#FFF";
-      diceDots[2].style.backgroundColor = "#ea412b";
-      diceDots[3].style.backgroundColor = "#FFF";
-      diceDots[4].style.backgroundColor = "#FFF";
-      diceDots[5].style.backgroundColor = "#FFF";
-      diceDots[6].style.backgroundColor = "#ea412b";
-      diceDots[7].style.backgroundColor = "#FFF";
-      diceDots[8].style.backgroundColor = "#ea412b";
+      dotsColor(diceDots, [0,2,6,8]);
       break;
 
     case 5:
-      diceDots[0].style.backgroundColor = "#ea412b";
-      diceDots[1].style.backgroundColor = "#FFF";
-      diceDots[2].style.backgroundColor = "#ea412b";
-      diceDots[3].style.backgroundColor = "#FFF";
-      diceDots[4].style.backgroundColor = "#ea412b";
-      diceDots[5].style.backgroundColor = "#FFF";
-      diceDots[6].style.backgroundColor = "#ea412b";
-      diceDots[7].style.backgroundColor = "#FFF";
-      diceDots[8].style.backgroundColor = "#ea412b";
+      dotsColor(diceDots, [0,2,4,6,8]);
       break;
 
     case 6:
-      diceDots[0].style.backgroundColor = "#ea412b";
-      diceDots[1].style.backgroundColor = "#FFF";
-      diceDots[2].style.backgroundColor = "#ea412b";
-      diceDots[3].style.backgroundColor = "#ea412b";
-      diceDots[4].style.backgroundColor = "#FFF";
-      diceDots[5].style.backgroundColor = "#ea412b";
-      diceDots[6].style.backgroundColor = "#ea412b";
-      diceDots[7].style.backgroundColor = "#FFF";
-      diceDots[8].style.backgroundColor = "#ea412b";
+      dotsColor(diceDots, [0,2,3,5,6,8]);
       break;
   }
 
   return diceNumber;
+}
+
+function dotsColor(diceDots, orangeDots) {
+  for (let i=0; i<9; i++) {
+    if (orangeDots.includes(i)) {
+      diceDots[i].style.backgroundColor = "#ea412b";
+    }
+    else {
+      diceDots[i].style.backgroundColor = "#FFF";
+    }
+  }
 }
 
 export {rollDice};
